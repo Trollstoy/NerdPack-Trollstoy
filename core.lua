ea700dc33ff2bc32839f043e9032e4429faa5971
Trollstoy <444796+Trollstoy@users.noreply.github.com>
2018-08-13 16:11:35 -0400

    Got rid of name handle

--- core.lua
+++ core.lua
@@ -1,9 +1,9 @@
-local n_name, Trollstoy = ...
+local _, Trollstoy = ...
 local NeP = NeP
 
 Trollstoy.Version = "1.0.0"
 Trollstoy.Branch = "RELEASE"
-Trollstoy.Name = "NerdPack - Starter Trollstoy Routine"
+Trollstoy.Name = "NerdPack - Trollstoy Routines"
 Trollstoy.Author = "Trollstoy"
 
 local frame = CreateFrame("GameTooltip", "Main_ScanningTooltip", UIParent, "GameTooltipTemplate")
